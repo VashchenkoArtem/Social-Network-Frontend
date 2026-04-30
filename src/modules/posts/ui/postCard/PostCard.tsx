b7346a49400f9ae4bcbe8a502c6b3bd0b31b89ee
Egor <139051554+EgorGalkinORG@users.noreply.github.com>
2026-04-30 18:13:58 +0300

    myPosts

---
+++ src/modules/posts/ui/postCard/PostCard.tsx
@@ -0,0 +1,40 @@
+import { View, Image, Text, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+import { useContext } from "react";
+import { UserContext } from "@modules/auth/context/user-context";
+import { COLORS } from "@shared/constants/colors";
+import { ICONS } from "@shared/ui";
+import { Redirect } from "expo-router";
+import { IProps } from "./types";
+
+
+export function PostCard(props: IProps){
+    const { post } = props
+    return (
+        <View style={styles.postContainer}>
+            <View style={styles.postHeader}>
+                <View style={styles.postAvatarSignatureInfo}>
+                    <View style={styles.postAvatarInfo}>
+                        <Image style={styles.authorAvatar} source={
+							post.author.avatars[post.author.avatars?.length - 1]
+							? { uri: `http://192.168.0.104:8000/media/thumb/${post.author.avatars[post.author.avatars.length - 1]?.filename}`, }
+							: require("../../../../assets/defaultAvatar.png")
+					}/>
+                        <Text style={styles.authorName}>{post.author.nickname}</Text>
+                    </View>
+                    { post.author.signature && 
+                    <Image style={styles.authorSignature} source={{
+                        uri: `http://192.168.0.104:8000/media/thumb/${post.author.signature}`
+                    }}/>}
+                </View>
+                <TouchableOpacity style={styles.dotIconContainer} >
+                    <ICONS.DotsIcon color={COLORS.gray} />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.postContent}>
+                <Text style={styles.postTitle}>{ post.title }</Text>
+                <Text style={styles.postDescription}>{ post.content }</Text>
+            </View>
+        </View>
+    )
+}
---
+++ src/modules/posts/ui/postCard/styles.ts
@@ -0,0 +1,86 @@
+import { COLORS } from "@shared/constants/colors";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    postContainer: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: COLORS.white,
+        borderWidth: 1,
+        borderColor: COLORS.gray,
+        borderRadius: 10, 
+        marginHorizontal: 6,
+        marginTop: 8,
+        gap: 8, 
+    },
+    
+    postHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderBottomColor: COLORS.gray,
+        borderBottomWidth: 1,
+        padding: 16
+    },
+
+    authorAvatar: {
+        width: 46,
+        height: 46,
+        borderRadius: 123
+    },
+
+    postAvatarSignatureInfo: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+    },
+    
+    postAvatarInfo: {
+        flexDirection: 'row',
+        // width: '100%',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        gap: 10
+    },
+
+    authorName: {
+        fontSize: 16,
+        fontWeight: 500, 
+        textAlign: 'left',
+        color: COLORS.black
+    },
+    authorSignature: {
+        width: 130,
+        height: 50
+    },
+
+    postTitle: {
+        fontSize: 16,
+        fontWeight: 500, 
+        textAlign: 'left',
+        color: COLORS.black
+    },
+    
+    postContent: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        // alignItems: 'center',
+        width: '100%', 
+        paddingHorizontal: 16,
+        paddingVertical: 16,
+        gap: 16, 
+    },
+
+    dotIconContainer: {
+		width: 20,
+        height: 20
+		// alignItems: 'flex-end'
+	},
+    postDescription: {
+        fontSize: 14,
+        fontWeight: 400, 
+        textAlign: 'left',
+        color: COLORS.black
+    }
+
+})
