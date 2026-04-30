4dd079264a9312ed8843a98d4f8e5558dfd6dda6
Artem <artemvaschenko83@gmail.com>
2026-04-30 19:52:04 +0300

    Fixing

--- src/app/(tabs)/posts.tsx
+++ src/app/(tabs)/posts.tsx
@@ -1,9 +1,10 @@
+import { MyPostsPage } from "@modules/posts/ui/myPosts/myPosts";
 import { View, Text } from "react-native";
 
 export default function PostsPage() {
 	return (
 		<View>
-			<Text>Сторінка публікації</Text>
+			<MyPostsPage></MyPostsPage>
 		</View>
 	);
 }
