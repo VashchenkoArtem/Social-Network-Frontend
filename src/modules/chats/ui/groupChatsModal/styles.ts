3cb05b1d481d7b8fb46a52d3ddfa62b40447e08d
Artem <artemvaschenko83@gmail.com>
2026-05-22 19:59:57 +0300

    WIP

---
+++ src/modules/chats/ui/groupChatsModal/styles.ts
@@ -0,0 +1,111 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "@shared/constants/colors";
+
+export const styles = StyleSheet.create({
+    modal: {
+        margin: 0,
+        // justifyContent: 'flex-end',
+        // marginBottom: 100
+    },
+
+    groupChatContainer: {
+        flexDirection: "column",
+        justifyContent: "space-between",
+        borderBlockColor: COLORS.lightGray,
+        borderRadius: 10,
+        borderWidth: 1,
+        flex: 1,
+        backgroundColor: COLORS.white,
+        paddingVertical: 16,
+        paddingHorizontal: 16,
+    },
+
+    groupChatHeader: {
+        height: 58,
+        borderBottomWidth: 1,
+        borderColor: COLORS.lightGray,
+        paddingBottom: 10,
+
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "flex-start"
+    },
+
+    close: {
+        fontSize: 20,
+        color: COLORS.lightGray
+    },
+
+    infoHeaderContainer: {
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        gap: 10
+    },
+
+    chatInfo: {
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        gap: 5
+    },
+
+    chatName: {
+        fontSize: 24,
+        fontWeight: 500,
+        color: COLORS.black
+    },
+
+    chatOnlineStatus: {
+        fontSize: 14,
+        fontWeight: 400,
+        color: COLORS.lightGray
+    },
+
+
+    inputMessageContainer: {
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: 'center',
+        gap: 24
+    },
+
+    messageBtnContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 16,
+        marginBottom: 16
+    },
+
+    menuContainer: {
+        position: "absolute",
+        top: 40,
+        right: 0,
+        width: 310,
+        backgroundColor: COLORS.preWhite,        
+        borderRadius: 10,
+        paddingVertical: 16,
+        paddingHorizontal: 16,
+        gap: 16,
+        zIndex: 1
+    },
+
+    menuBtn: {
+        width: '100%',
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10
+    },
+
+    divider: {
+        height: 1,
+        backgroundColor: COLORS.lightGray
+    },
+
+    menuBtnText: {
+        color: COLORS.black,
+        fontSize: 16,
+        fontWeight: 500
+    }
+});
